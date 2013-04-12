7fbd018c99bd771f4b2038cfba3ae10d5f3c477a
Renato Florentino Garcia <fgarcia.renato@gmail.com>
2013-04-11 23:22:10 -0300

    Created EnumWidget.

--- src/imgproc/cvt_color.cpp
+++ src/imgproc/cvt_color.cpp
@@ -1,101 +1,94 @@
 #include "cvt_color.hpp"
 
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-#include <cstring>
-#include <memory>
-
 #include "enums.hpp"
 #include "utility/conversions.hpp"
+#include "widget/enum_widget.hpp"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <memory>
+#include <boost/optional/optional.hpp>
+#include <boost/tuple/tuple.hpp>
 
 namespace
 {
-struct Options
-{
-    int code;
-};
+    typedef boost::tuple<int> Arguments;
 
-std::auto_ptr<Options> cvtColor_dialog()
-{
-    gimp_ui_init("opencv", FALSE);
-
-    GimpDialog* dialog =
-        GIMP_DIALOG(gimp_dialog_new("cvtColor",
-                                    "cvtColor_role",
-                                    NULL,
-                                    GTK_DIALOG_MODAL,
-                                    gimp_standard_help_func,
-                                    "opencv_gimp",
-
-                                    GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                                    GTK_STOCK_OK,     GTK_RESPONSE_OK,
-
-                                    NULL));
-
-    GtkHBox* gtkHBox_1 = GTK_HBOX(gtk_hbox_new(FALSE, 0));
-
-    GimpEnumComboBox* gimpEnumComboBox_1 =
-        GIMP_ENUM_COMBO_BOX(gimp_enum_combo_box_new(TYPE_CVT_COLOR_ENUM));
-    gimp_int_combo_box_set_active(GIMP_INT_COMBO_BOX(gimpEnumComboBox_1), cv::COLOR_BGR2BGRA);
-
-    GtkLabel* gtkLabel_1 = GTK_LABEL(gtk_label_new("code:"));
-
-    gtk_container_add(GTK_CONTAINER(gtkHBox_1), GTK_WIDGET(gtkLabel_1));
-    gtk_container_add(GTK_CONTAINER(gtkHBox_1), GTK_WIDGET(gimpEnumComboBox_1));
-    gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
-                      GTK_WIDGET(gtkHBox_1));
-
-    gtk_widget_show_all(GTK_WIDGET(dialog));
-
-    std::auto_ptr<Options> options;
-    if (gimp_dialog_run(GIMP_DIALOG(dialog)) == GTK_RESPONSE_OK)
+    boost::optional<Arguments> presentDialog()
     {
-        options.reset(new Options());
+        gimp_ui_init("opencv", FALSE);
 
-        gint value;
-        gimp_int_combo_box_get_active(GIMP_INT_COMBO_BOX(gimpEnumComboBox_1), &value);
-        options->code = value;
+        GimpDialog* dialog =
+            GIMP_DIALOG(gimp_dialog_new("cvtColor",
+                                        "cvtColor_role",
+                                        NULL,
+                                        GTK_DIALOG_MODAL,
+                                        gimp_standard_help_func,
+                                        "opencv_gimp",
+
+                                        GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                        GTK_STOCK_OK,     GTK_RESPONSE_OK,
+
+                                        NULL));
+
+        EnumWidget enumWidget(TYPE_CVT_COLOR_ENUM, cv::COLOR_BGR2BGRA);
+
+        GtkHBox* gtkHBox_1 = GTK_HBOX(gtk_hbox_new(FALSE, 0));
+        GtkLabel* gtkLabel_1 = GTK_LABEL(gtk_label_new("code:"));
+        gtk_container_add(GTK_CONTAINER(gtkHBox_1), GTK_WIDGET(gtkLabel_1));
+        gtk_container_add(GTK_CONTAINER(gtkHBox_1), enumWidget);
+
+        gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dialog))),
+                          GTK_WIDGET(gtkHBox_1));
+
+        gtk_widget_show_all(GTK_WIDGET(dialog));
+
+        boost::optional<Arguments> arguments;
+        if (gimp_dialog_run(GIMP_DIALOG(dialog)) == GTK_RESPONSE_OK)
+        {
+            arguments = Arguments(enumWidget);
+        }
+
+        gtk_widget_destroy(GTK_WIDGET(dialog));
+
+        return arguments;
     }
-
-    gtk_widget_destroy(GTK_WIDGET(dialog));
-
-    return options;
-}
 
 } // namespace
 
 void imgproc::cvtColor::install()
 {
     static GimpParamDef args[] =
-    {
-        {GIMP_PDB_INT32,    (gchar*)"run-mode", (gchar*)"Run mode"},
-        {GIMP_PDB_IMAGE,    (gchar*)"image",    (gchar*)"Input image"},
-        {GIMP_PDB_DRAWABLE, (gchar*)"drawable", (gchar*)"Input drawable"}
-    };
+        {
+            {GIMP_PDB_INT32,    (gchar*)"run-mode", (gchar*)"Run mode"},
+            {GIMP_PDB_IMAGE,    (gchar*)"image",    (gchar*)"Input image"},
+            {GIMP_PDB_DRAWABLE, (gchar*)"drawable", (gchar*)"Input drawable"}
+        };
 
     gimp_install_procedure("cvtColor",
-        "OpenCV color converter",
-        "OpenCV color converter",
-        "Renato Florentino Garcia",
-        "Renato Florentino Garcia",
-        "2013",
-        "_cvtColor",
-        "RGB*, GRAY*",
-        GIMP_PLUGIN,
-        G_N_ELEMENTS(args), 0,
-        args, NULL);
+                           "OpenCV color converter",
+                           "OpenCV color converter",
+                           "Renato Florentino Garcia",
+                           "Renato Florentino Garcia",
+                           "2013",
+                           "_cvtColor",
+                           "RGB*, GRAY*",
+                           GIMP_PLUGIN,
+                           G_N_ELEMENTS(args), 0,
+                           args, NULL);
 }
 
-void imgproc::cvtColor::run(GimpDrawable *drawable)
+void imgproc::cvtColor::run(GimpDrawable* drawable)
 {
-    std::auto_ptr<Options> options = cvtColor_dialog();
-    if (options.get() == NULL)
+    boost::optional<Arguments> arguments = presentDialog();
+    if (!arguments)
     {
         return;
     }
-    cv::Mat cvImage = drawableToMat(drawable);
-    cv::Mat tmp;
-    cv::cvtColor(cvImage, tmp, options->code);
-    setMatToDrawable(tmp, drawable);
+
+    cv::Mat src = drawableToMat(drawable);
+    cv::Mat dst;
+    cv::cvtColor(src, dst, boost::get<0>(*arguments));
+    setMatToDrawable(dst, drawable);
 }
---
+++ src/widget/enum_widget.hpp
@@ -0,0 +1,32 @@
+#ifndef _WIDGET_ENUM_WIDGET_HPP_
+#define _WIDGET_ENUM_WIDGET_HPP_
+
+#include <glib-object.h>
+
+class EnumWidget
+{
+public:
+    EnumWidget(GType enumType, gint defaultItem)
+        : gimpEnumComboBox(GIMP_ENUM_COMBO_BOX(gimp_enum_combo_box_new(enumType)))
+    {
+        gimp_int_combo_box_set_active(GIMP_INT_COMBO_BOX(this->gimpEnumComboBox), defaultItem);
+    }
+
+    operator GtkWidget*()
+    {
+        return GTK_WIDGET(this->gimpEnumComboBox);
+    }
+
+    operator int() const
+    {
+        gint value;
+        gimp_int_combo_box_get_active(GIMP_INT_COMBO_BOX(this->gimpEnumComboBox), &value);
+
+        return value;
+    }
+
+private:
+    GimpEnumComboBox* gimpEnumComboBox;
+};
+
+#endif /* _WIDGET_ENUM_WIDGET_HPP_ */
